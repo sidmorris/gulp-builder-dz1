14b93b1a76bf18a5203407e6c6abc2a134725fb9
sidmorris <sidmorris@mail.ru>
2018-02-26 18:50:40 +0300

    svg edit color

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,8 +1,9 @@
 'use strict';
 
-module.exports = function() {
-  $.gulp.task('sass', function() {
+module.exports = function () {
+  $.gulp.task('sass', function () {
     return $.gulp.src('./source/style/app.scss')
+      .pipe($.gp.sassGlob())
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
